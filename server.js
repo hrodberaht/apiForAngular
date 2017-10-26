0ac64f896680709504e30f556d90e8af33909edb
Robert <seedraev@gmail.com>
2017-10-26 19:22:54 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", function(req,res){
+    res.send("hello");
+});
+
+app.listen(8081, function(){
+    
+    console.log("Server start's");
+    
+});   
